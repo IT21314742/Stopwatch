52158636d326a395215b1f4069308e41920f06b8
IT21314742 <it21314742@my.sliit.lk>
2024-03-10 06:54:40 +0530

    Update JS
    Update JS

--- script.js
+++ script.js
@@ -16,4 +16,5 @@ function stopwatch(){
     let h = hours < 10 ? "0" + hours : hours;
     let m = minutes < 10 ? "0" + minutes : minutes;
     let s = seconds < 10 ? "0" + seconds : seconds;
+    
 }
