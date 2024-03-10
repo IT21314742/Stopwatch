2d46c7e310c252d7288a8ee67b8640d029e4e752
IT21314742 <it21314742@my.sliit.lk>
2024-03-10 07:15:14 +0530

    Update JS
    add js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let [seconds, minutes, hours] = [0,0,0];
+let [seconds, minutes, hours] = [0, 0, 0];
 let displayTime = document.getElementById("displayTime");
 let timer = null;
 
-function stopwatch(){
+function stopwatch() {
     seconds++;
-    if(seconds == 60){
+    if (seconds == 60) {
         seconds = 0;
         minutes++;
-        if(minutes == 60){
+        if (minutes == 60) {
             minutes = 0;
             hours++;
         }
@@ -17,13 +17,12 @@ function stopwatch(){
     let m = minutes < 10 ? "0" + minutes : minutes;
     let s = seconds < 10 ? "0" + seconds : seconds;
 
-    displayTime.innerHTML = h +":"+ m +":"+ s;
+    displayTime.innerHTML = h + ":" + m + ":" + s;
 }
 
-function watchStart(){
-    if(timer!== null)
-    {
+function watchStart() {
+    if (timer !== null) {
         clearInterval(timer);
     }
-    timer = setInterval(stopwatch,1000);
+    timer = setInterval(stopwatch, 1000);
 }
